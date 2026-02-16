43314530c1416b8713523037e90c9cacf1005822
Adrian <adrian.sokolovic10@gmail.com>
2026-02-16 10:01:36 +0100

    kalkulator

---
+++ Kalkulator/java.js
@@ -0,0 +1,73 @@
+let AC = document.querySelectorAll("button")[0];
+let DEL = document.querySelectorAll("button")[1];
+let dijeljenje = document.querySelectorAll("button")[2];
+let puta = document.querySelectorAll("button")[3];
+let btn7 = document.querySelectorAll("button")[4];
+let btn8 = document.querySelectorAll("button")[5];
+let btn9 = document.querySelectorAll("button")[6];
+let minus = document.querySelectorAll("button")[7];
+let btn4 = document.querySelectorAll("button")[8];
+let btn5 = document.querySelectorAll("button")[9];
+let btn6 = document.querySelectorAll("button")[10];
+let plus = document.querySelectorAll("button")[11];
+let btn1 = document.querySelectorAll("button")[12];
+let btn2 = document.querySelectorAll("button")[13];
+let btn3 = document.querySelectorAll("button")[14];
+let jednako = document.querySelectorAll("button")[15];
+let btn0 = document.querySelectorAll("button")[16];
+let dec = document.querySelectorAll("button")[17];
+
+AC.addEventListener("click", function (){
+
+});
+DEL.addEventListener("click", function (){
+    
+});
+dijeljenje.addEventListener("click", function (){
+    
+});
+puta.addEventListener("click", function (){
+    
+});
+btn7.addEventListener("click", function (){
+    
+});
+btn8.addEventListener("click", function (){
+    
+});
+btn9.addEventListener("click", function (){
+    
+});
+minus.addEventListener("click", function (){
+    
+});
+btn4.addEventListener("click", function (){
+    
+});
+btn5.addEventListener("click", function (){
+    
+});
+btn6.addEventListener("click", function (){
+    
+});
+plus.addEventListener("click", function (){
+    
+});
+btn1.addEventListener("click", function (){
+    
+});
+btn2.addEventListener("click", function (){
+    
+});
+btn3.addEventListener("click", function (){
+    
+});
+jednako.addEventListener("click", function (){
+    
+});
+btn0.addEventListener("click", function (){
+    
+});
+dec.addEventListener("click", function (){
+    
+});
