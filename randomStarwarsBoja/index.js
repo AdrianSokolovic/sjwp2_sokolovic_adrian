f66431caa8e888539165ad65f3fe61998c5396e2
AdrianSokolovic <adrian.sokolovic10@gmail.com>
2026-03-02 10:24:04 +0100

    qr i skola

---
+++ randomStarwarsBoja/index.js
@@ -0,0 +1,7 @@
+import names from "starwars-names";
+var randomName = names.random();
+
+import randomColor from "randomcolor";
+var color = randomColor();
+
+console.log(`Moje ime je ${randomName}, a najdraža boja je ${color}`);
